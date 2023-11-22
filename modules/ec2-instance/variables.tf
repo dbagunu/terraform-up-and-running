63883f933ff921b6ecb7d12a652518007a94c75b
dbagunu <dcmbagunu@tutanota.com>
2023-11-22 14:24:13 +0800

    Initial Build

---
+++ modules/ec2-instance/variables.tf
@@ -0,0 +1,24 @@
+# Instances variables
+variable "ami_id" {
+  description = "Uses the Ubuntu Server 22.04 LTS ami"
+  type = string
+  default = "ami-078c1149d8ad719a7"
+}
+variable "instance_type" {
+  description = "This is the free tier instance type"
+  type = string
+  default = "t2.micro"
+}
+variable "tag_name" {
+  default = {
+    Name = "test-instance"
+  }
+  type = map(string)
+}
+
+# Security group variables
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+  type = number
+}

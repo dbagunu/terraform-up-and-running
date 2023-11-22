9da8655e3e4cff717f52b9c35b44621a5161a9fa
dbagunu <dcmbagunu@tutanota.com>
2023-11-22 15:12:43 +0800

    Removed security group from main.tf

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -1,18 +1,8 @@
 resource "aws_instance" "Ubuntu" {
   ami = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.test-sg.id] # Creates an implicit dependency on SG
+  vpc_security_group_ids = [var.sg_id] # Creates an implicit dependency on SG
   tags = var.tag_name
   user_data = file("init.sh")
   user_data_replace_on_change = true
-
-}
-resource "aws_security_group" "test-sg" {
-  name = "instance-sg"
-  ingress {
-    from_port        = var.server_port
-    to_port          = var.server_port
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
 }
--- modules/ec2-instance/variables.tf
+++ modules/ec2-instance/variables.tf
@@ -17,8 +17,6 @@ variable "tag_name" {
 }
 
 # Security group variables
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  default = 8080
-  type = number
+variable "sg_id" {
+  description = "security group id"
 }

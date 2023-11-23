ceb205110bbaaa7e89e0112a412a9e6af3ca6040
dbagunu <dcmbagunu@tutanota.com>
2023-11-23 16:34:34 +0800

    renamed file

--- Chapter-3-workspace-isolation/file-layout/stage/services/webserver-cluster/main-stage-webserver.tf
+++ Chapter-3-workspace-isolation/file-layout/stage/services/webserver-cluster/main-stage-webserver.tf
@@ -12,22 +12,22 @@
 
 # Deploys Instances under ALB
 module "test_ALB" {
-  depends_on = [module.security_group]
-  source     = "../../../modules/services/load-balancer"
-  sg_id      = module.security_group.sg_id
-  alb_sg     = module.security_group.alb_sg
-  user_data = "init.sh"
-  cluster_name = var.cluster_name
+  depends_on        = [module.security_group]
+  source            = "../../../modules/services/load-balancer"
+  sg_id             = module.security_group.sg_id
+  alb_sg            = module.security_group.alb_sg
+  user_data         = "init.sh"
+  cluster_name      = var.cluster_name
   s3_tfstate_bucket = "terraform-bucketforlearning4"
-  s3_key_tfstate = "stage/data-stores/mysql/terraform.state"
+  s3_key_tfstate    = "stage/data-stores/mysql/terraform.state"
 }
 
 module "security_group" {
-  source = "../../../modules/services/security-group"
+  source       = "../../../modules/services/security-group"
   cluster_name = var.cluster_name
 }
 
 output "alb_dns_name" {
-  value = module.test_ALB.alb_dns_name
+  value       = module.test_ALB.alb_dns_name
   description = "The domain name of the load balancer"
 }
